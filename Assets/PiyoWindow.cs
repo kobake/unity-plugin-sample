5007aef4368d64953a29a99f7d7cae6b058f4a47
kobake <kobake@user.sourceforge.net>
2014-12-18 05:41:46 +0900

    レイアウトラップ例

--- Assets/PiyoWindow.cs
+++ Assets/PiyoWindow.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 
 public class PiyoWindow : EditorWindow
 {
+	bool m_dame = true;
 	void OnGUI()
 	{
 		GUILayout.Label("Hoge");
@@ -18,7 +19,7 @@ public class PiyoWindow : EditorWindow
 		{
 			Debug.Log("B!!!");
 		}
-		
+
 		GUILayout.BeginHorizontal();
 		GUILayout.Button("1");
 		GUILayout.Button("2");
@@ -30,5 +31,60 @@ public class PiyoWindow : EditorWindow
 		GUILayout.Button("5");
 		GUILayout.Button("6");
 		GUILayout.EndHorizontal();
+
+		H(delegate()
+		{
+			H(delegate()
+			{
+				H(delegate()
+				{
+					H(delegate()
+					{
+						V(delegate()
+						{
+							GUILayout.Button("a");
+							GUILayout.Button("b");
+						});
+						V(delegate()
+						{
+							GUILayout.Button("c");
+							GUILayout.Button("d");
+						});
+					});
+					V(delegate()
+					{
+						GUILayout.Button("x");
+						GUILayout.Button("y");
+					});
+					GUILayout.Button("z");
+				});
+			});
+		});
+	}
+	public void H(Action action)
+	{
+		GUILayout.BeginHorizontal("box");
+		try
+		{
+			action();
+		}
+		catch (Exception ex)
+		{
+			Debug.Log(ex.ToString());
+		}
+		GUILayout.EndHorizontal();
+	}
+	public void V(Action action)
+	{
+		GUILayout.BeginVertical("box");
+		try
+		{
+			action();
+		}
+		catch (Exception ex)
+		{
+			Debug.Log(ex.ToString());
+		}
+		GUILayout.EndVertical();
 	}
 }
