965e57b0ff69400c1c38342f8ced776a3c36d495
kobake <kobake@user.sourceforge.net>
2014-12-18 05:04:39 +0900

    ウィンドウ表示とか

---
+++ Assets/PiyoWindow.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+
+public class PiyoWindow : EditorWindow
+{
+}
---
+++ Assets/TestDesu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class TestDesu {
+	[MenuItem("Hoge/Fuga")]
+	public static void Fugafuga()
+	{
+		Debug.Log("Hello");
+		PiyoWindow.GetWindow(typeof(PiyoWindow));
+	}
+}
