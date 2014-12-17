cd32c099a7bae8f3e4094eece5ffd463b3ac108a
kobake <kobake@user.sourceforge.net>
2014-12-18 05:18:48 +0900

    レイアウト周り

--- Assets/PiyoWindow.cs
+++ Assets/PiyoWindow.cs
@@ -3,7 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEditor;
+using UnityEngine;
 
 public class PiyoWindow : EditorWindow
 {
+	void OnGUI()
+	{
+		GUILayout.Label("Hoge");
+		if (GUILayout.Button("FugaA"))
+		{
+			Debug.Log("A!!!");
+		}
+		if (GUILayout.Button("FugaB"))
+		{
+			Debug.Log("B!!!");
+		}
+		
+		GUILayout.BeginHorizontal();
+		GUILayout.Button("1");
+		GUILayout.Button("2");
+		GUILayout.Button("3");
+		GUILayout.EndHorizontal();
+
+		GUILayout.BeginHorizontal();
+		GUILayout.Button("4");
+		GUILayout.Button("5");
+		GUILayout.Button("6");
+		GUILayout.EndHorizontal();
+	}
 }
